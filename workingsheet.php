b3c7b92586f52758bc8919448cedc7ee314ca50d
javier monleon <dumbo.cat@hotmail.com>
2022-01-25 00:29:56 +0000

    first commit

---
+++ workingsheet.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="styles.css/styles.css"  />
+   
+    <title>Workingsheet</title>
+   
+</head>
+<body >
+  <div class="background" >
+    
+      <div class="central" >
+        <div class="row">
+          <div class="col">
+            <div class="fs-2 lh-1 fw-bold">
+              <p>JM Electronáutica S.L.</p>
+            </div>
+            <div class="fs-5 lh-1">
+              Muelle deportivo Las Palmas</br>
+              Tel: (+34)654535612</br>
+              B05448063</br>
+              info@jmelectronautica.com</br>
+            </div>
+          </div>
+          <div class="col">
+            <div class="logo">
+              <img id="logo" src="images/logo.png"  class="float-end" alt="JMElectronautica" >
+            </div>
+          </div>    
+        </div>
+    
+        <div class="row">
+          <table id="firstRow"> 
+            <tr>
+              <td id="firstRow1">PROYECTO Nº</td>
+              <td id="firstRow2">   </td>
+              <td id="firstRow3">Nombre Embarcación</td>
+              <td id="firstRow4">   </td>
+            </tr>
+          </table>
+        </div>      
+        <div class="row"  >
+          <table id="secondRow">
+            <tr>
+              <td id="secondRow1">Fecha</td>
+              <td id="secondRow2">   </td>
+              <td id="secondRow3">Nombre</td>
+              <td id="secondRow4">   </td>
+              <td id="secondRow5">Email</td>
+              <td id="secondRow6">   </td>
+            </tr>
+          </table>
+          <table id="thirdRow"> 
+            <tr> 
+              <td id="thirdRow1">Marina</td>
+              <td id="thirdRow2">   </td>
+              <td id="thirdRow3">Dirección</td>
+              <td id="thirdRow4">  </td>
+            </tr>
+          </table> 
+          <table id="thirdRow"> 
+            <tr> 
+              <td id="fourthRow1">Forma de pago</td> 
+              <td id="fourthRow2">    </td>
+              <td id="fourthRow3">País</td>
+              <td id="fourthRow4">   </td>
+              <td id="fourthRow5">Teléfono</td>
+              <td id="fourthRow6">   </td>
+              <td id="fourthRow7">DNI/Pass</td>
+              <td id="fourthRow8">   </td>
+            </tr>
+          </table>
+        </div>   
+        <div class="row">
+          <table id="fifthRow">
+            <tr>
+              <td id="fifthRow1">Trabajos a realizar</td>
+            </tr>
+            <tr> 
+              <td id="fifthRow2"></td>
+            </tr>
+          </table>
+        </div>
+        <div class="row">
+          <table id="sixthRow">   
+            <tr>
+              <td id="sixthRow1">Fecha</td>
+              <td id="sixthRow2">Descripción</td>
+              <td id="sixthRow3">Hora entrada</td>
+              <td id="sixthRow3">Hora salida</td>
+              <td id="sixthRow3">Tiempo efectivo</td>
+            </tr>
+          </table>
+        </div>
+        <div class="row">
+          <div class="seventhRow">
+            <table id="seventhRow">   
+              <tr>
+               <td class="align-top" id="seventhRow1">01-01-2022</td>
+               <td class="align-top"id="seventhRow2">Reparacion piloto automatico en puerto, Salir a navegar para comprobar estado de piloto, y verificar funcionamiento</td>
+                <td class="align-top"id="seventhRow3">09:00</td>
+                <td class="align-top"id="seventhRow3">14:00</td>
+                <td class="align-top"id="seventhRow3">4</td>
+             </tr>
+            </table>
+          </div>
+        </div>
+        <div class="row">
+          <div class="eighthRow">
+            <table id="eighthRow">   
+              <tr>
+               <td class="align-top" id="eighthRow1"></td>
+               <td class="align-top"id="eighthRow2"></td>
+                <td class="align-top"id="eighthRow3">Total horas</td>
+                
+                <td class="align-top"id="eighthRow5">4</td>
+             </tr>
+            </table>
+          </div>
+        </div>
+        <div class="row">
+          <table id="ninethRow">
+            <tr>
+              <td id="ninethRow1">Ovservaciones. Material utilizado</td>
+            </tr>
+            <tr> 
+              <td class="align-top" id="ninethRow2"></td>
+            </tr>
+          </table>
+        </div>
+        <div class="row">
+          <table id="tenthRow">
+            <tr>
+              <td id="tenthRow1">firma tecnico</td><td id="tenthRow2">firma cliente</td>
+            </tr>
+            
+          </table>
+        </div>
+      </div>
+  </div>    
+  
+ 
+      
+    
+
+
+
+
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</html>
+
+
+<?php
+?>
