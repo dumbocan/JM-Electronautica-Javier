654171e42b85ffe6435149428b9274f3b9015869
javier monleon <dumbo.cat@hotmail.com>
2022-01-27 00:19:48 +0000

    workingsheet finish

---
+++ invoice.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="styles.css/styles.css"  />
+   
+    <title>Workingsheet</title>
+   
+</head>
+<body >
+  <div class="background" >
+    
+      <div class="central">
+        <div class="header">  
+          <div class="row">
+            <div class="col">
+              <div class="fs-1 lh-3 fw-bold">
+                JM Electronáutica S.L.
+              </div>
+              <div class="lh-1">
+                Muelle deportivo Las Palmas</br>
+                Tel: (+34)654535612</br>
+                B05448063</br>
+                info@jmelectronautica.com</br>
+              </div>
+            </div>
+            <div class="col">
+              <div class="logo">
+                <img id="logo" src="images/logo.png"  class="float-end" alt="JMElectronautica" >
+              </div>
+            </div>    
+          </div>
+               
+          <div class="row">
+            <table id="firstRow"> 
+              <tr>
+                <td id="firstRow1">FACTURA Nº</td>
+                <td id="firstRow2">   </td>
+                <td id="firstRow3">Nombre Embarcación</td>
+                <td id="firstRow4">   </td>
+              </tr>
+            </table>
+          </div>      
+          <div class="row"  >
+            <table id="secondRow">
+              <tr>
+                <td id="secondRow1">Fecha</td>
+                <td id="secondRow2">   </td>
+                <td id="secondRow3">Nombre</td>
+                <td id="secondRow4">   </td>
+                <td id="secondRow5">Email</td>
+                <td id="secondRow6">   </td>
+              </tr>
+            </table>
+            <table id="thirdRow"> 
+              <tr> 
+                <td id="thirdRow1">Marina</td>
+                <td id="thirdRow2">   </td>
+                <td id="thirdRow3">Dirección</td>
+                <td id="thirdRow4">  </td>
+              </tr>
+            </table> 
+            <table id="thirdRow"> 
+              <tr> 
+                <td id="fourthRow1">Forma de pago</td> 
+                <td id="fourthRow2">    </td>
+                <td id="fourthRow3">País</td>
+                <td id="fourthRow4">   </td>
+                <td id="fourthRow5">Teléfono</td>
+                <td id="fourthRow6">   </td>
+                <td id="fourthRow7">DNI/Pass</td>
+                <td id="fourthRow8">   </td>
+              </tr>
+            </table>
+          </div>
+        </div>    
+        <div class="resume">
+          <div class="row">
+            <table id="sixthRow">   
+              <tr>
+                <td id="sixthRow1">Cantidad</td>
+                <td id="sixthRow2">Descripción</td>
+                <td id="sixthRow3-1">Precio unitario</td>
+                <td id="sixthRow3-2">Desc.</td>
+                <td id="sixthRow3-1">Subtotal </td>
+              </tr>
+            </table>
+          </div>
+          <div class="row">
+            <div class="seventhRow">
+              <table id="seventhRow">   
+                <tr class="align-top">
+                <td  id="seventhRow1"></td>
+                <td id="seventhRow2"></td>
+                  <td id="seventhRow3-1"></td>
+                  <td id="seventhRow3-2"></td>
+                  <td id="seventhRow3-1"></td>
+              </tr>
+              </table>
+            </div>
+          </div>
+          <div class="row">
+            <div class="eighthRow-1">
+              <table id="eighthRow">   
+                <tr>
+                  <td  id="eighthRow1-1"></td>
+                  <td id="eighthRow2"></td>
+                  <td id="eighthRow3"></td>
+                  <td id="eighthRow5"></td>
+                </tr>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div> 
+  </div>
+
+
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</html>
+
+
+<?php
+?>
